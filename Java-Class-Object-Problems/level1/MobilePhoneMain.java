3e4723451048596b6c00589e439d4a096a05c010
Teeshakul <teesha119@gmail.com>
2025-09-02 17:33:14 +0530

    [MobilePhone] Add MobilePhone program with details display

---
+++ Java-Class-Object-Problems/level1/MobilePhoneMain.java
@@ -0,0 +1,6 @@
+public class MobilePhoneMain {
+    public static void main(String[] args) {
+        MobilePhone phone1 = new MobilePhone("Samsung", "Galaxy S23", 79999);
+        phone1.displayDetails();
+    }
+}
