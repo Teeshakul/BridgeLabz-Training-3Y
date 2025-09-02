2c925501a88017ea6ef150eb0212680e65108671
Teeshakul <teesha119@gmail.com>
2025-09-02 17:31:04 +0530

    [Circle] Add Circle program with area and circumference calculation

---
+++ Java-Class-Object-Problems/level1/Circle.java
@@ -0,0 +1,21 @@
+public class Circle {
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    double calculateCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    void displayDetails() {
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + calculateArea());
+        System.out.println("Circumference: " + calculateCircumference());
+    }
+}
---
+++ Java-Class-Object-Problems/level1/CircleMain.java
@@ -0,0 +1,6 @@
+public class CircleMain {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(7);
+        c1.displayDetails();
+    }
+}
