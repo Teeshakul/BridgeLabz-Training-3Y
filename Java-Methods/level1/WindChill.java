8e0977afc97e721a3205769212982c0f72917b99
Teeshakul <teesha119@gmail.com>
2025-09-03 23:54:41 +0530

    [Teesha] Added Wind Chill program

---
+++ Java-Methods/level1/WindChill.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class WindChill {
+    public static double calculate(double t, double v) {
+        return 35.74 + 0.6215 * t + (0.4275 * t - 35.75) * Math.pow(v, 0.16);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter temperature (F): ");
+        double t = sc.nextDouble();
+        System.out.print("Enter wind speed (mph): ");
+        double v = sc.nextDouble();
+        System.out.println("Wind chill = " + calculate(t, v));
+        sc.close();
+    }
+}
