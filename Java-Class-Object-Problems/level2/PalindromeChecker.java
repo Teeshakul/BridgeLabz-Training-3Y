e2e48dca87e4baf99d2f4a1d1f2f3ca62e3fa382
Teeshakul <teesha119@gmail.com>
2025-09-03 14:58:01 +0530

    [Teesha] Add PalindromeChecker program

---
+++ Java-Class-Object-Problems/level2/PalindromeChecker.java
@@ -0,0 +1,20 @@
+public class PalindromeChecker {
+    String text;
+
+    PalindromeChecker(String text) {
+        this.text = text;
+    }
+
+    boolean isPalindrome() {
+        String reversed = new StringBuilder(text).reverse().toString();
+        return text.equalsIgnoreCase(reversed);
+    }
+
+    void displayResult() {
+        if (isPalindrome()) {
+            System.out.println(text + " is a palindrome.");
+        } else {
+            System.out.println(text + " is not a palindrome.");
+        }
+    }
+}
---
+++ Java-Class-Object-Problems/level2/PalindromeCheckerMain.java
@@ -0,0 +1,6 @@
+public class PalindromeCheckerMain {
+    public static void main(String[] args) {
+        PalindromeChecker p1 = new PalindromeChecker("madam");
+        p1.displayResult();
+    }
+}
