3c818bf40c905b38bc9543984dada32aaebef238
Teeshakul <teesha119@gmail.com>
2025-09-03 17:06:38 +0530

    [Teesha] Add Person Class With Copy Constructor

---
+++ Java-Constructors/level1/Constructors/PersonMain.java
@@ -0,0 +1,10 @@
+public class PersonMain {
+    public static void main(String[] args) {
+        Person p1 = new Person("Alice", 25);
+        Person p2 = new Person(p1);
+
+        p1.display();
+        p2.display();
+    }
+}
+
