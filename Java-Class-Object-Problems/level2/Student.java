c24641ffffd0370e35dbfe29abb56f649b7c541a
Teeshakul <teesha119@gmail.com>
2025-09-03 14:56:50 +0530

    [Teesha] Add Student program with grade calculation

---
+++ Java-Class-Object-Problems/level2/Student.java
@@ -0,0 +1,25 @@
+public class Student {
+    String name;
+    int rollNumber;
+    double marks;
+
+    Student(String name, int rollNumber, double marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+
+    char calculateGrade() {
+        if (marks >= 90) return 'A';
+        else if (marks >= 75) return 'B';
+        else if (marks >= 50) return 'C';
+        else return 'D';
+    }
+
+    void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + calculateGrade());
+    }
+}
---
+++ Java-Class-Object-Problems/level2/StudentMain.java
@@ -0,0 +1,6 @@
+public class StudentMain {
+    public static void main(String[] args) {
+        Student s1 = new Student("Teesha", 101, 88.5);
+        s1.displayDetails();
+    }
+}
