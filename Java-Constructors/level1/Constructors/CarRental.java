fda08db02345a52b5da41ecda08b24ba5be85fe3
Teeshakul <teesha119@gmail.com>
2025-09-03 17:09:50 +0530

    [Teesha] Add CarRental Class With Cost Calculation

---
+++ Java-Constructors/level1/Constructors/CarRental.java
@@ -0,0 +1,20 @@
+public class CarRental {
+    String customerName;
+    String carModel;
+    int rentalDays;
+
+    public CarRental(String customerName, String carModel, int rentalDays) {
+        this.customerName = customerName;
+        this.carModel = carModel;
+        this.rentalDays = rentalDays;
+    }
+
+    public double calculateTotalCost() {
+        double ratePerDay = 1000;
+        return rentalDays * ratePerDay;
+    }
+
+    public void displayDetails() {
+        System.out.println("Customer: " + customerName + ", Car: " + carModel + ", Days: " + rentalDays + ", Total Cost: " + calculateTotalCost());
+    }
+}
---
+++ Java-Constructors/level1/Constructors/CarRentalMain.java
@@ -0,0 +1,6 @@
+public class CarRentalMain {
+    public static void main(String[] args) {
+        CarRental rental1 = new CarRental("Bob", "Honda City", 5);
+        rental1.displayDetails();
+    }
+}
