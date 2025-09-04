5fad0d245d272af9a3eb5056d3a887e79e715914
Teeshakul <teesha119@gmail.com>
2025-09-04 10:08:22 +0530

    [Teesha] Added IllegalArgumentException Demo program

---
+++ Java-String/level1/IllegalArgumentDemo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class IllegalArgumentDemo {
+    static void generate(String s) {
+        System.out.println(s.substring(5, 2));
+    }
+    static void handle(String s) {
+        try {
+            System.out.println(s.substring(5, 2));
+        } catch (IllegalArgumentException e) {
+            System.out.println("Handled: " + e);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        handle(s);
+        sc.close();
+    }
+}
