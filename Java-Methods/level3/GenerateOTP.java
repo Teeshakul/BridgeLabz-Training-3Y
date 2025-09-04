7c5afe6923cf83f0586ffa3f1bc20b6517378ca8
Teeshakul <teesha119@gmail.com>
2025-09-04 09:43:14 +0530

    [Teesha] Added Generate OTP program

---
+++ Java-Methods/level3/GenerateOTP.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class GenerateOTP{
+    public static void main(String[] args) {
+        Random r = new Random();
+        int otp = 100000 + r.nextInt(900000);
+        System.out.println("OTP: " + otp);
+    }
+}
