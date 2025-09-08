05589479a310dc828d438bc2906240bfa49abbfe
Teeshakul <teesha119@gmail.com>
2025-09-08 14:44:27 +0530

    [Teesha] Added BankAccount program

---
+++ Java-Static-Final/BankAccount.java
@@ -0,0 +1,43 @@
+public class BankAccount {
+    private static String bankName = "State Bank of India";
+    private static int totalAccounts = 0;
+    private final String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public BankAccount(String accountHolderName, String accountNumber, double balance) {
+        this.accountHolderName = accountHolderName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        totalAccounts++;
+    }
+
+    public static void getTotalAccounts() {
+        System.out.println("Total number of accounts: " + totalAccounts);
+    }
+
+    public void displayAccountDetails() {
+        if (this instanceof BankAccount) {
+            System.out.println("Bank Name: " + bankName);
+            System.out.println("Account Holder: " + accountHolderName);
+            System.out.println("Account Number: " + accountNumber);
+            System.out.println("Balance: $" + balance);
+        }
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) balance -= amount;
+    }
+
+    public static void main(String[] args) {
+        BankAccount a1 = new BankAccount("Lynda", "ACC123", 1000);
+        BankAccount a2 = new BankAccount("Prince", "ACC456", 500);
+        a1.displayAccountDetails();
+        a2.displayAccountDetails();
+        getTotalAccounts();
+    }
+}
