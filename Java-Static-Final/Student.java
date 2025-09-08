a2a04cfea84278c3594c8d9f27a106851cf59909
Teeshakul <teesha119@gmail.com>
2025-09-08 14:45:38 +0530

    [Teesha] Added Student program

---
+++ Java-Static-Final/Student.java
@@ -0,0 +1,46 @@
+public class Student {
+    private static String universityName;
+    private static int totalStudents = 0;
+    private final int rollNumber;
+    private String name;
+    private char grade;
+
+    public Student(String name, int rollNumber, char grade) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+        totalStudents++;
+    }
+
+    public static void setUniversityName(String name) {
+        universityName = name;
+    }
+
+    public static void displayTotalStudents() {
+        System.out.println("Total Students: " + totalStudents);
+    }
+
+    public void displayStudentDetails() {
+        if (this instanceof Student) {
+            System.out.println("University: " + universityName);
+            System.out.println("Roll: " + rollNumber);
+            System.out.println("Name: " + name);
+            System.out.println("Grade: " + grade);
+        }
+    }
+
+    public void updateGrade(char newGrade) {
+        this.grade = newGrade;
+    }
+
+    public static void main(String[] args) {
+        Student.setUniversityName("Global University");
+        Student s1 = new Student("Hemashree", 101, 'A');
+        Student s2 = new Student("Sharmila", 102, 'B');
+        displayTotalStudents();
+        s1.displayStudentDetails();
+        s2.displayStudentDetails();
+        s2.updateGrade('A');
+        s2.displayStudentDetails();
+    }
+}
