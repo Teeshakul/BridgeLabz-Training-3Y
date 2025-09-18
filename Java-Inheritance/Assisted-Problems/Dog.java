e04b8cbcef30d224ff8b7426ee3dd577764767cf
Teeshakul <teesha119@gmail.com>
2025-09-18 11:09:32 +0530

    [Teesha] Add Animal Heirarchy Problem

---
+++ Java-Inheritance/Assisted-Problems/Dog.java
@@ -0,0 +1,10 @@
+class Dog extends Animal {
+    Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println(name + " barks: Woof Woof!");
+    }
+}
